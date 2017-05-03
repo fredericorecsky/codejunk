b734a888d9df0321277aabe3b2f46538a0f975bc
fredericorecsky <cartas@frederico.me>
2017-05-03 19:51:15 +0200

    no explanation

---
+++ PZXY/lib/PZXY.pm
@@ -0,0 +1,15 @@
+package PZXY;
+
+use strict;
+use warnings;
+
+use Module::Pluggable 
+    search_path =>'PZXY',
+    sub_name => 'commands',
+    require =>1;
+
+
+
+
+
+1;
---
+++ PZXY/lib/PZXY/Test.pm
@@ -0,0 +1,12 @@
+package PZXY::ssh_config_list_hosts;
+
+sub name {
+    return "ssh_config_list_hosts";
+}
+
+sub run{
+    print "Prints the list of all ssh hosts\n";
+}
+
+
+1;
